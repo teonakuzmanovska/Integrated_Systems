54f136c46bf17ac60c466de3f6995e03dfd7fc42
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-06-03 23:22:13 +0200

    aud3 -> aud4
    added new projects for onion architecture
    - added interface and implementation folders in service and repository

---
+++ is_aud1/EShop.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EShop.Repository
+{
+    public class Class1
+    {
+    }
+}
---
+++ is_aud1/EShop.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EShop.Service
+{
+    public class Class1
+    {
+    }
+}
