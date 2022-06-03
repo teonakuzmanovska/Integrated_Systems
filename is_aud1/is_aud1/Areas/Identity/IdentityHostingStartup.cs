da4385fb0cd188befadcda2d06c22c6978581136
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-06-03 02:11:58 +0200

    updated aud2
    - scaffolded Identity Register and Login

---
+++ is_aud1/is_aud1/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using is_aud1.Data;
+using is_aud1.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(is_aud1.Areas.Identity.IdentityHostingStartup))]
+namespace is_aud1.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
