75c714a332069528e75b0d80634bd556e67dad16
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-06-03 03:45:50 +0200

    updated aud3
    - added ShoppingCartDto for showing shopping cart
    * AddToShoppingCartDto was just for sending data through "add to cart" button
    - completed mvc for ShoppingCart

--- is_aud1/is_aud1/Controllers/ShoppingCartController.cs
+++ is_aud1/is_aud1/Controllers/ShoppingCartController.cs
@@ -1,8 +1,11 @@
 ﻿using is_aud1.Data;
+using is_aud1.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace is_aud1.Controllers
@@ -17,7 +20,37 @@ namespace is_aud1.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var user = _context.Users.Where(z => z.Id == userId)
+                .Include("UserShoppingCart.ProductsInShoppingCarts")
+                .Include("UserShoppingCart.ProductsInShoppingCarts.Product")
+                .FirstOrDefault();
+
+            var userShoppingCart = user.UserShoppingCart;
+
+            var productList = userShoppingCart.ProductsInShoppingCarts.Select(z => new
+            {
+                Quantity = z.Quantity,
+                ProductPrice = z.Product.ProductPrice
+            });
+
+            int totalPrice = 0;
+
+            foreach (var item in productList)
+            {
+                totalPrice += item.ProductPrice * item.Quantity;
+            }
+
+            // some kind of constructor - "set method option"
+            ShoppingCartDto model = new ShoppingCartDto
+            {
+                ProductsInShoppingCarts = userShoppingCart.ProductsInShoppingCarts.ToList(),
+                TotalPrice = totalPrice
+
+            };
+
+            return View(model);
         }
     }
 }
---
+++ is_aud1/is_aud1/Models/ShoppingCartDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is_aud1.Models
+{
+    public class ShoppingCartDto
+    {
+        public List<ProductsInShoppingCart> ProductsInShoppingCarts { get; set; }
+        public int TotalPrice { get; set; }
+    }
+}
