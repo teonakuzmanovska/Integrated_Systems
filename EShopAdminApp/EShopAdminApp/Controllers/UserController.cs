d344538999f3da568c9564f8c30452e6fb7b229d
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-06-06 03:58:44 +0200

    finished aud8
    - added UserClass in original application
    - added function for importing excel file in UserController

--- EShopAdminApp/EShopAdminApp/Controllers/UserController.cs
+++ EShopAdminApp/EShopAdminApp/Controllers/UserController.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using EShopAdminApp.Models;
+using ExcelDataReader;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace EShopAdminApp.Controllers
 {
@@ -17,8 +23,58 @@ namespace EShopAdminApp.Controllers
 
         public IActionResult ImportUsers(IFormFile file)
         {
-            // TO-DO
-            return null;
+            //make a copy
+            string pathToUpload = $"{Directory.GetCurrentDirectory()}\\files\\{file.FileName}";
+
+            using (FileStream fileStream = System.IO.File.Create(pathToUpload))
+            {
+                file.CopyTo(fileStream);
+                fileStream.Flush();
+            }
+
+            //read data from uploaded file
+
+            List<User> users = GetUsersFromExcelFile(file.FileName);
+
+            HttpClient client = new HttpClient();
+
+            string URL = "https://localhost:44359/api/AdminApi/ImportAllUsers";
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(users), Encoding.UTF8, "application/json");
+           
+            HttpResponseMessage response = client.PostAsync(URL, content).Result;
+
+            var result = response.Content.ReadAsAsync<bool>().Result;
+
+            return RedirectToAction("Index", "Order");
+        }
+
+        private List<User> GetUsersFromExcelFile(string fileName)
+        {
+            string pathToFile = $"{Directory.GetCurrentDirectory()}\\files\\{fileName}";
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            
+            List<User> userList = new List<User>();
+
+            using (var stream = System.IO.File.Open(pathToFile, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    while (reader.Read())
+                    {
+                        userList.Add(new Models.User
+                        {
+                            Email = reader.GetValue(0).ToString(),
+                            Password = reader.GetValue(1).ToString(),
+                            ConfirmPassword = reader.GetValue(2).ToString()
+                        });
+                    }
+                }
+            }
+
+            return userList;
+
         }
     }
 }
