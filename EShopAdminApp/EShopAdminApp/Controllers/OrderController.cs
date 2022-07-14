dd1e2430064319b30a02f16b9def2ff8d211cc2f
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2022-07-15 01:59:47 +0200

    aud8 -> aud9
    - added exportOrders action in OrderController (Admin)

--- EShopAdminApp/EShopAdminApp/Controllers/OrderController.cs
+++ EShopAdminApp/EShopAdminApp/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using EShopAdminApp.Models;
+﻿using ClosedXML.Excel;
+using EShopAdminApp.Models;
 using GemBox.Document;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -93,7 +94,52 @@ namespace EShopAdminApp.Controllers
             var stream = new MemoryStream();
             document.Save(stream, new PdfSaveOptions());
 
+            //DocxSaveOptions() i .docx za word dokument
+
             return File(stream.ToArray(), new PdfSaveOptions().ContentType, "ExportInvoice.pdf");
+        }
+
+        [HttpGet]
+        public FileContentResult ExportAllOrders()
+        {
+            string fileName = "Orders.xlsx";
+            string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+            using (var workbook = new XLWorkbook())
+            {
+                IXLWorksheet worksheet = workbook.Worksheets.Add("All Orders");
+                worksheet.Cell(1, 1).Value = "Order Id";
+                worksheet.Cell(1, 2).Value = "Customer email";
+
+                HttpClient client = new HttpClient();
+                string URL = "https://localhost:44359/api/AdminApi/GetAllActiveOrders";
+
+                HttpResponseMessage responseMessage = client.GetAsync(URL).Result;
+
+                var data = responseMessage.Content.ReadAsAsync<List<Order>>().Result;
+
+                for(int i = 1; i < data.Count(); i++)
+                {
+                    var item = data[i - 1];
+
+                    worksheet.Cell(i + 1, 1).Value = item.Id.ToString();
+                    worksheet.Cell(i + 1, 2).Value = item.OrderedBy.Email;
+
+                    for(int p=1; p <= item.Products.Count(); p++)
+                    {
+                        worksheet.Cell(1, p + 3).Value = "Product-" + (p);
+                        worksheet.Cell(i + 1, p + 3).Value = item.Products.ElementAt(p - 1).Product.ProductName;
+                    }
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+
+                    return File(content, contentType, fileName);
+                }
+            }
         }
     }
 }
